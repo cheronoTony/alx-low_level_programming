a20d31879ec9385cad450792f6e9155235c78d3a
cheronoTony <anthonykimutai63@gmail.com>
2023-02-22 01:59:44 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_times_table_(3);
+    _putchar('\n');
+    print_times_table_(5);
+    _putchar('\n');
+    print_times_table_(98);
+    _putchar('\n');
+    print_times_table_(12);  
+    return (0);
+}
